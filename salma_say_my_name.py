014120e78d873351e5366090f183babbbe9fad10
Salma <salmaibrahim813@gmail.com>
2023-02-03 15:43:02 +0000

    added changes to aaliyah repo

---
+++ salma_say_my_name.py
@@ -0,0 +1,9 @@
+# This is a variable
+first_name = 'Salma'
+# This is a variable
+last_name = 'Ibrahim'
+
+# The print function will concatenate my two variables and give me my full name as the output
+
+print(first_name + " " + last_name)
+
