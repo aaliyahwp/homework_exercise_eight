fe2d2d7717dc9e2de6c8d8be1e638fe0e9f7ca05
Salma <salmaibrahim813@gmail.com>
2023-02-03 15:49:07 +0000

    added a variable to Aaliyah code

--- say_my_name.py
+++ say_my_name.py
@@ -3,5 +3,7 @@ first_name = 'Aaliyah'
 # This is also a variable containing my last name
 last_name = "Wais-Pompey"
 
-# here we have concatonated out first and last name with a space
-print (first_name +" "+ last_name)
+full_name = first_name + " "+ last_name
+
+# here we have concatenated out first and last name with a space
+print (full_name)
